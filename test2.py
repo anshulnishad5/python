08ee94e92a9e90fd133d9f1c6d1f9eec9b0a37ca
anshulnishad5 <anshulnishad525@gmail.com>
2025-09-09 01:46:20 +0530

    Delete test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-#even and odd numbers
-def even_odd_numbers(n):
-    even_numbers = [x for x in range(n) if x % 2 == 0]
-    odd_numbers = [x for x in range(n) if x % 2 !=0]
-    return even_numbers, odd_numbers
-
-print(even_odd_numbers(10))
-
-# Output: ([0, 2, 4, 6, 8], [1, 3, 5, 7, 9])s
